0357a666aee6ba729b6cfa76e62a4a4d2a2cb472
Rhomadon <adonrhomadon100@gmail.com>
2022-10-06 11:12:10 +0700

    component within

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import './App.css';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import { MapContainer, TileLayer } from 'react-leaflet'
+import Within from './components/Within';
 
 function App() {
+
+  const center = [-6.233628818303135, 106.82149988475815]
+
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+    <MapContainer center={center} zoom={11} scrollWheelZoom={true} minZoom={2}>
       <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
+      <Within/>
     </MapContainer>
   )
 }
